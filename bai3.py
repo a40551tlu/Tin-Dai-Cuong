61bd2427e47cc1851cc6ffe6cb8e96c5205a281d
a40551tlu <75386726+a40551tlu@users.noreply.github.com>
2020-12-07 16:37:00 +0700

    Update bai3.py

--- bai3.py
+++ bai3.py
@@ -1,7 +1,7 @@
-a = int(input("nhap vao a: "))
-b = int(input("nhap vao b: "))
+a = float(input("nhap vao a: "))
+b = float(input("nhap vao b: "))
 if b == 0:
   print("so khong hop le.")
 else:
-  print("phan nguyen: ", a / b)
-  print("phan du: ", a % b)
+  print("phan nguyen: ", int(a / b))
+  print("phan du: ", int(a % b))
