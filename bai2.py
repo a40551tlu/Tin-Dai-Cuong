488309a750521fc729b2c81669e91e3356df191d
a40551tlu <75386726+a40551tlu@users.noreply.github.com>
2020-12-07 16:21:57 +0700

    Update bai2.py

--- bai2.py
+++ bai2.py
@@ -1,6 +1,13 @@
 a = int(input("Nhap vao thang: "))
+b = int(input("Nhap vao nam: "))
 if a == 2:
-  print("thang co 29 hoac 28 ngay")
+  if ((b % 100) == 0) and ((b % 400) == 0):
+    print("Co 29 ngay.")
+  else:
+    if ((b % 4 == 0) and (b % 100 != 0)):
+      print("co 29 ngay.")
+    else:
+      print("Co 28 ngay.")
 elif a in [1,3,5,7,8,10,12] :
   print("Thang co 31 ngay.")
 else :
