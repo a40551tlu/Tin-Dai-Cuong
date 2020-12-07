304d9420a771816c4afe9d7f09d6431b65249c90
a40551tlu <75386726+a40551tlu@users.noreply.github.com>
2020-12-07 14:28:37 +0700

    Create GPTB1.py

---
+++ GPTB1.py
@@ -0,0 +1,10 @@
+a = int(input("Nhap so a: "))
+b = int(input("Nhap so b: "))
+
+if a == 0:
+    print("Phuong trinh vo nghiem. \n")
+else :
+  if b == 0:
+    print("Phuong trinh co vo so nghiem ")
+  else :
+      print("Phuong trinh co nghiem la: ", -b / a)
