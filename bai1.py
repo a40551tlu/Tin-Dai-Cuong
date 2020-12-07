98802c339da162103d79ddd44620677a8ac75194
a40551tlu <75386726+a40551tlu@users.noreply.github.com>
2020-12-07 15:44:13 +0700

    Update bai1.py

--- bai1.py
+++ bai1.py
@@ -1,5 +1,7 @@
 a = int(input("nhap nam: "))
-if ((a % 4 == 0) and (a % 100 != 0)):
+if (a < 100) and (a % 4 == 0):
+	print(a, "la nam nhuan")
+elif ((a % 4 == 0) and (a % 100 != 0)):
   print(a, " la nam nhuan.")
 else:
   print(a, " khong la nam nhuan.")
